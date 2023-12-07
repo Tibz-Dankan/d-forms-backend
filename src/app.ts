ead84968f8fef2e3b966147efb3033997e6a2881
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-12-07 13:23:45 +0300

    feat: setup d-forms-server

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import logger from "morgan";
+import http from "http";
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(cors({ origin: process.env.FRONTEND_URL }));
+} else {
+  app.use(cors());
+}
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+// app.use("/api/v1/users", userRoutes);
+
+// app.use(errorController);
+
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Endpoint not found!",
+  });
+});
+
+const PORT = 8000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`D-forms server running on port ${PORT}`);
+});
